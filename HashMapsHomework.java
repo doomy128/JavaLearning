d3555b01f4dd199f09195c509080becc086a9843
Jared <rissler@orphan.(none)>
2012-10-25 15:42:30 -0500

    Added HashMapsHomework

---
+++ HashMapsHomework.java
@@ -0,0 +1,18 @@
+
+public class HashMapsHomework {
+	//1. Create and declare a HashMap
+	
+	//2. Put an object in a HashMap
+	
+	//3. Get an object from a HashMap
+	
+	//4. Create and declare a 2D HashMap
+	
+	//5. Put a HashMap in the 2D HashMap,then put 
+	// something in that HashMap
+	
+	//BONUS: I want a method that returns a HashMap that's
+	//a "mirror" of the given HashMap(Keys become values
+	//and vice-versa)Good luck!
+	
+}
